8421a29ffd2f73fc3feb4811357c058f92da2d10
Paul Eustice <paul.eustice@gmail.com>
2020-11-23 11:57:19 +0000

    centralise diagrams styles and animate dependencies

--- projects/slides/src/app/slides/components/large-application/large-application.component.ts
+++ projects/slides/src/app/slides/components/large-application/large-application.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-large-app',
   templateUrl: './large-application.component.html',
-  styleUrls: [ './large-application.component.scss' ],
+  styleUrls: [ './large-application.component.scss', '../../../../diagrams.scss' ],
 })
 export class LargeApplicationComponent {
   example = 0;
--- projects/slides/src/app/slides/components/module-federation/module-federation.component.ts
+++ projects/slides/src/app/slides/components/module-federation/module-federation.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-module-federation',
   templateUrl: './module-federation.component.html',
-  styleUrls: [ './module-federation.component.scss' ],
+  styleUrls: [ './module-federation.component.scss', '../../../../diagrams.scss' ],
 })
 export class ModuleFederationComponent implements OnInit {
+  openRemotes: Set<number> = new Set();
+  accessingDeps: Set<string> = new Set();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  openRemote(id: number): void {
+    this.openRemotes.add(id);
+  }
+
+  accessDeps(ids: Array<string>): void {
+    ids.forEach((id) => {
+      this.accessingDeps.add(id);
+      setTimeout(() => this.accessingDeps.delete(id), 5000);
+    });
+  }
 }
